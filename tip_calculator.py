0ac172226c88f8e055a8018e206417087f804ca9
Marquan Jones <mtjones1115@gmail.com>
2021-10-16 09:11:19 -0400

    completed tip calculator on day two

---
+++ tip_calculator.py
@@ -0,0 +1,12 @@
+# one hundred days of code day two
+
+sub_total = float(input("What was the total bill? \n"))
+tip_percentage = int(input("What percentage tip would you like to give? \n"))
+bill_split = int(input("How many poeple to split the bill? \n"))
+
+bill_total = sub_total*(1+tip_percentage/100)/bill_split
+tidy_total = round(bill_total, 2)
+message = f"Each person should pay: {tidy_total}"
+
+print(message)
+
