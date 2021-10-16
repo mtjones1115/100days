524317c4c06bb5aa140554af5ca92b854713de72
Marquan Jones <mtjones1115@gmail.com>
2021-10-16 09:42:23 -0400

    updated final total to always include two decimal places

--- tip_calculator.py
+++ tip_calculator.py
@@ -6,7 +6,7 @@ bill_split = int(input("How many poeple to split the bill? \n"))
 
 bill_total = sub_total*(1+tip_percentage/100)/bill_split
 tidy_total = round(bill_total, 2)
-tidy_total = "{:.2f}".format()
+tidy_total = "{:.2f}".format(tidy_total)
 message = f"Each person should pay: {tidy_total}"
 
 print(message)
